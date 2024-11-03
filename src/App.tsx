68f223d27397e05a5061dc007fae4e61fe98d4da
Esteban Forero <estebanmff@outlook.com>
2024-11-03 01:58:22 -0500

    Finished chat mockup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import ChatScreen from "./components/chatScreen"
+
+function App() {
+
+  return (
+    <div className="bg-black w-full h-screen flex justify-center align-middle">
+      <ChatScreen/>          
+    </div>
+  )
+}
+
+export default App
